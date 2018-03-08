99814c19ff30a6592addd8ac7faa0933a574de03
Charity Cardinal <charcard@umich.edu>
2018-03-08 16:48:48 -0500

    First commit for deliverable two

---
+++ script.js
@@ -0,0 +1,63 @@
+//Exercise One:
+var num=prompt("Enter a number");
+var sum=0;
+for(var i=0;i<=num;i++){
+      sum+=i;
+      }
+      console.log("Output: " + sum);
+
+/*Exercise Two:
+var continueplaying=false;
+var result=" ";
+var play=prompt("Do you want to play?");
+if(play=="yes"){
+continueplaying=true;
+}
+else{
+	console.log("Output: " + result);
+}
+do {
+var word=prompt("Enter a word"); 
+result+=word;
+var other=prompt("Do you want to play again?");
+if(other=="no"){
+console.log("Output: " + result);
+continueplaying=false;
+}
+} while(continueplaying==true);
+*/
+
+/*Exercise Three:
+var exclaim="!"
+var name="Hello.  My name is Charity";
+var continueplaying=false;
+var play=prompt("Would you like to print your name?")
+while(play=="yes"){
+continueplaying=true;
+	console.log(name);
+var other=prompt("Would you like to print your name again?");
+name+=exclaim;
+}
+*/
+
+/*Exercise Four:
+var play=prompt("What time of day is it?");
+if(play=="morning"){
+var eggs="Since it is morning, you should be eating breakfast. We suggest eggs and toast.";
+	console.log(eggs);
+}
+else if(play=="noon"){
+var play=prompt("What time of day is it?");
+var salad="Since it is noon, you should be eating lunch. We suggest a salad.";
+	console.log(salad);
+}
+else if (play=="evening"){
+var play=prompt("What time of day is it?");
+var chicken="Since it is evening, you should be eating dinner. We suggest chicken and rice.";
+	console.log(chicken);
+}
+*/
+
+
+	
+	
